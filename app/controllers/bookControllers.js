c1db92c9983403034eb53b6361f90a24a3a60ad7
Juan Jose Arango <jjad85@gmail.com>
2020-05-30 13:10:47 -0500

    Adición funcionalidad para correr en angular

--- app/controllers/bookControllers.js
+++ app/controllers/bookControllers.js
@@ -1,24 +1,23 @@
-const serviceBook = require("../services/bookServices");
-const serviceUser = require("../services/userServices");
+const serviceBook = require('../services/bookServices');
 
 exports.getAllBooks = async (req, res) => {
-	const allBooks = await serviceBook.getAllBooksExist(req.query);
-	res.status(201).send(allBooks);
+    const allBooks = await serviceBook.getAllBooksExist(req.query);
+    res.status(201).send(allBooks);
 };
 
 exports.createBook = async (req, res) => {
-	const book = await serviceBook.createOneBook(req.body);
-	if (!book) {
-		res.status(500).send({ error: "El libro no ha podido ser creado" });
-	}
-	res.status(201).send(book);
+    const book = await serviceBook.createOneBook(req.body);
+    if (!book) {
+        res.status(500).send({ error: 'El libro no ha podido ser creado' });
+    }
+    res.status(201).send(book);
 };
 
 exports.getBookId = async (req, res) => {
-	let infoBookId = await (
-		await serviceBook.getBookToId(req.user, req.params.libroId)
-	).toJSON();
-	/*if (req.user) {
+    let infoBookId = await (
+        await serviceBook.getBookToId(req.user, req.params.libroId)
+    ).toJSON();
+    /*if (req.user) {
 		const infoUser = await serviceUser.getUserToId(req.user);
 		infoBookId.isFavorite = "False";
 		for (i = 0; i < infoUser.favoritos.length; i++) {
@@ -27,33 +26,33 @@ exports.getBookId = async (req, res) => {
 			}
 		}
 	}*/
-	res.status(201).send(infoBookId);
+    res.status(201).send(infoBookId);
 };
 
 exports.patchBookId = async (req, res) => {
-	let infoBookPatch = await serviceBook.patchBookToId(
-		req.params.libroId,
-		req.body
-	);
-	res.status(201).send(infoBookPatch);
+    let infoBookPatch = await serviceBook.patchBookToId(
+        req.params.libroId,
+        req.body
+    );
+    res.status(201).send(infoBookPatch);
 };
 
 exports.removeBookId = async (req, res) => {
-	let infoBookRemove = await serviceBook.removeBookToId(req.params.libroId);
-	res.status(201).send(infoBookRemove);
+    let infoBookRemove = await serviceBook.removeBookToId(req.params.libroId);
+    res.status(201).send(infoBookRemove);
 };
 
 exports.addFavorite = async (req, res) => {
-	let infoUserResult;
-	if (req.user) {
-		console.log(req.user);
-		/*let infoUser = await (await serviceUser.getUserToId(req.user)).populate(
+    let infoUserResult;
+    if (req.user) {
+        console.log(req.user);
+        /*let infoUser = await (await serviceUser.getUserToId(req.user)).populate(
 			book
 		);*/
-		infoUserResult = await serviceBook.addBookFavorite(
-			req.user,
-			req.params.libroId
-		);
-	}
-	res.status(201).send(infoUserResult);
+        infoUserResult = await serviceBook.addBookFavorite(
+            req.user,
+            req.params.libroId
+        );
+    }
+    res.status(201).send(infoUserResult);
 };
